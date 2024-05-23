af8364348ed363a0f10f905e3370cfe4b1105a17
Mayurrr29 <112242386+Mayurrr29@users.noreply.github.com>
2024-05-23 22:01:55 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+            $('.top').show();
+        }else{
+            $('.top').hide();
+        }
+    });
+
+    // Smooth Scrolling
+
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+           scrollTop : $($(this).attr('href')).offset().top, 
+        },
+        500,
+        'linear'
+        );
+    });
+
+});
+
+
+
+
+// Animate on Scroll
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll=()=> {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+
+        if(top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    });
+}
+
+
+
+// Website theme changer (light/dark)
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "sun.png";
+    }else{
+        icon.src = "moon.png";
+    }
+}
